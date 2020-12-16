68d18666f94d19671c124488dd74deee326334d7
rsvanarnam <36892758+rsvanarnam@users.noreply.github.com>
2020-12-16 11:46:28 -0700

    Initial Commit

---
+++ Bullseye SwiftUI/Bullseye_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye_SwiftUIApp.swift
+//  Bullseye SwiftUI
+//
+//  Created by Rebecca Van Arnam on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye SwiftUI
+//
+//  Created by Rebecca Van Arnam on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
